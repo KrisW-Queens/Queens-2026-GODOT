bd6fc04ce2a8da6f3e53905fa304ef5414419af4
KrisW-Queens <krisw@queens.school.nz>
2026-03-15 21:20:32 +1300

    End of Video 5
    Last video completed.
    Added background
    Added sounds

--- platform-game-sr-coder/scripts/player.gd
+++ platform-game-sr-coder/scripts/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @onready var animation : AnimatedSprite2D = $AnimatedSprite2D
+@onready var jumpsound: AudioStreamPlayer = $jumpsound
+@onready var deathsound: AudioStreamPlayer = $deathsound
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -14,6 +16,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jumpsound.play()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -45,4 +48,5 @@ func _physics_process(delta: float) -> void:
 		
 	
 func respawn():
+	deathsound.play()
 	position = start_position
